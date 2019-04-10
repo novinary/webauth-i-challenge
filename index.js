723fb11f968994fd9800154783ebea25ed3f4fb3
novinary <novina.dev@gmail.com>
2019-04-10 01:49:59 +0100

    Use cookies for logged in users

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+// cookies setup
+const session = require("express-session");
+
+const sessionConfig = {
+  name: 'monster',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 10,// milliseconds
+    secure: false, // use cookie over https
+    httpOnly: true, // false means can JS access the coookie on the client
+  },
+  resave: false, // avoid recreating existing sessions
+  saveUniinitialized: false, // GDPR compliance
+};
+
+server.use(session(sessionConfig));
+
+// endpoints
 server.get("/", (req, res) => {
   res.send("It's alive!");
 });
@@ -11,8 +29,9 @@ server.get("/", (req, res) => {
 // [POST] /api/register
 server.post("/api/register", (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, pass.username);
+  const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -24,20 +43,25 @@ server.post("/api/register", (req, res) => {
 
 // [POST] /api/login
 server.post("/api/login", (req, res) => {
-  let { username, password } = req.body;
-  //const hash = bcrypt.hashSync(user.password, 10);
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session is added by expression-session
+        req.session.user = user;
+        
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
 });
 
 // [GET]/api/users
@@ -48,6 +72,7 @@ server.get("/api/users", (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
 
 const port = process.env.PORT || 2500;
 server.listen(port, () =>
